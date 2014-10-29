892fb9f101494df68a3f3024ac0f955358d80056
nyy7 <niyanyan7@gmail.com>
2014-10-29 14:48:48 -0400

    update project3

--- project3.pl
+++ project3.pl
@@ -1,30 +1,71 @@
-/* helper functions */
+/**
+ * helper rules
+ * same(List1:list,List2:list)
+ * Succeeds if the second list is equal to the first list.
+ */ 
 same([],[]).
 same([H1|X],[H2|Y]) :- H1 = H2, same(X,Y).
+
+/**
+ * helper rules
+ * member(Key:atom,List1:list)
+ * Succeeds if the key exists in the list gave as a second parameter.
+ */ 
 member(X,[X|Y]).
 member(X,[_|Y]) :- member(X,Y).
+
+/**
+ * helper rules
+ * right(Left:atom,Right:atom,List1:list)
+ * Succeeds if both Left and Right exists in the List1, and Left is on the left of Right in that list.
+ */ 
 right(Left,Right,[Left,Right|_]).
 right(Left,Right,[_|Y]) :- right(Left,Right,Y).
+
+/**
+ * helper rules
+ * adjacent(X:atom,Y:atom,List1:list)
+ * Succeeds if both X and Y exists in the List1, and X, Y are next to each other in that list. 
+ */ 
 adjacent(X,Y,S) :- right(X,Y,S); right(Y,X,S).
+
+/** 
+ * helper rules
+ * are_unique(A:atom,B:atom,C:atom,D:atom,E:atom)
+ * Succeed if A,B,C,D,E are different from each other
+ */ 
 are_unique(A, B, C, D, E) :-
 	not(A=B),not(A=C), not(A=D),not(A=E),not(B=C),not(B=D),not(B=E),not(C=D),not(C=E),not(D=E).
+
+/**
+ * helper rules
+ * Succeed when the guess information(parameters) are correct in the range as given.
+ */ 
 guess_house(Nationality,Pet,Drink,Dessert) :-
 	member(Nationality,[englishman,japanese,norwegian,spaniard,ukrainian]),
 	member(Pet,[dog,fox,horse,snails,zebra]),
 	member(Drink,[coffee,milk,orange_juice,tea,water]),
 	member(Dessert,[ice_cream,pie,cake,chocolate,mousse]).
-/* guess */	
+
+/** guess
+ * Succeed when the guess result fullfil the clues provided.
+ *
+ * Examples:
+ *
+ * ?- house_solution(Blue_Nationality,Blue_Pet,Blue_Drink,Blue_Dessert,Green_Nationality,Green_Pet,Green_Drink,Green_Dessert,Ivory_Nationality,Ivory_Pet,Ivory_Drink,Ivory_Dessert,Red_Nationality,Red_Pet,Red_Drink,Red_Dessert,Yellow_Nationality,Yellow_Pet,Yellow_Drink,Yellow_Dessert).
+ * [Blue_Nationality = ukrainian, Blue_Pet = snails, Blue_Drink = tea, Blue_Dessert = ice_cream, Green_Nationality = spaniard, Green_Pet = dog, Green_Drink = coffee, Green_Dessert = cake, Ivory_Nationality = japanese, Ivory_Pet = horse, Ivory_Drink = milk, Ivory_Dessert = mousse, Red_Nationality = englishman, Red_Pet = fox, Red_Drink = orange_juice, Red_Dessert = chocolate, Yellow_Nationality = norwegian, Yellow_Pet = zebra, Yellow_Drink = water, Yellow_Dessert = pie]
+ */
 house_solution(
 	Blue_Nationality,Blue_Pet,Blue_Drink,Blue_Dessert,
 	Green_Nationality,Green_Pet,Green_Drink,Green_Dessert,
 	Ivory_Nationality,Ivory_Pet,Ivory_Drink,Ivory_Dessert,
 	Red_Nationality,Red_Pet,Red_Drink,Red_Dessert,
 	Yellow_Nationality,Yellow_Pet,Yellow_Drink,Yellow_Dessert) :-
-	    /* DEFINE STREET */
+	    /* Define street */
 		same([house(_,norwegian,_,_,_),_,
 		      house(_,_,_,milk,_),_,_],Street),
 		
-	    /* CLUES */
+	    /* Clues */
 		member(house(red,englishman,_,_,_),Street),
 		member(house(_,spaniard,dog,_,_),Street),
 		member(house(green,_,_,coffee,_),Street),
